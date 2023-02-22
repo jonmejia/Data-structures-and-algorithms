f225da522e2a088d2d5f494fc70548d68a685ae6
Jon <jonathan@dataminded.net>
2023-02-21 23:05:03 -0500

    finished recursive

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -12,6 +12,14 @@ def recursive_binary_search(list,target):
             else:
                 return recursive_binary_search(list[midpoint+1:])
 
+def verify(result):
+    print(f"Target found: {result}")
+
+num_list = [1,2,3,4,5,6,7,8,9,10]
+
+result = recursive_binary_search(num_list,6)
+verify(result)
+
 
 
     
