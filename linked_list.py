6375dc7bbb832cb288ca98416694cc9b0abf10d7
Jonathan <jonathan@dataminded.net>
2023-02-24 21:29:00 -0500

    fixed indentation errors

--- linked_list.py
+++ linked_list.py
@@ -1,67 +1,62 @@
 class Node:
-    
+
     """"
     An object for storing a single node of a linked list
-    
+
     Models two attributes, data and the link to the next node in the list"""
     data = None
     next_node = None
-    
-    def __init__(self,data):
+
+    def __init__(self, data):
         self.data = data
-        
+
     def __repr__(self):
         return "<Node data: %s>" % self.data
-    
+
+
 class linked_list:
     """
     Singly linked list
     """
-    
+
     def __init__(self):
         self.head = None
-        
-                
+
     def is_empty(self):
-        return(self.head ==None)
-    
+        return (self.head == None)
+
     def size(self):
         """returns the number of nodes in the lsit
-        
+
         takes 0(n) time
         """
         current = self.head
         count = 0
-        
+
         while current != None:
-            count +=1
+            count += 1
             current = current.next_node
-            
+
         return count
-    
-    def add(self,data):
+
+    def add(self, data):
         """
     adds a new node containg data at the head of the list
     takes o(n)"""
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
-        
-    def search(self,key):
-        """
-        Search for the first node containing data that matches the key
-        return the node or none if not found
-        
-        takes O(n)"""
-       current = self.head
-       while current:
-           if current.data == key:
+
+    def search(self, key):
+        current = self.head
+        while current:
+            if current.data == key:
                return current
-           else:
+            else:
                current = current.next_node 
-            return None
+        return None
         
-     def __repr__(self):
+    def __repr__(self):
         """Returns a string representation of the list
         takes O(n) time
         """
